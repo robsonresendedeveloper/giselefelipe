e3297d609e534135775c62547d60b5558f790246
Robson Resende <robson.souza.cl@gmail.com>
2020-02-04 14:28:48 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :users
+  resources :confirmations
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "confirmations#new"
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+end
