e773b51dd95f8748228cc98e92f9761b9429af7a
Robson Resende <robson.souza.cl@gmail.com>
2020-02-04 16:12:14 -0300

    making banners

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'font_awesome5_rails'
+
+gem 'twitter-bootstrap-rails'
+
+gem 'jquery-rails'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use Puma as the app server
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,37 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require jquery3
 //= require activestorage
-//= require turbolinks
 //= require_tree .
+
+$("document").ready(function(){
+  $(".banner2").hide();
+  $(".banner3").hide();
+
+	var img=1;
+	setInterval(function(){
+		switch(img) {
+			case 1:
+				img=2;
+				$(".banner1").fadeOut(1600, function(){
+          			$(".banner2").fadeIn(1600);
+				});
+			break;
+			case 2:
+				img=3;
+				$(".banner2").fadeOut(1600, function(){
+					$(".banner3").fadeIn(1600);
+				});
+			break;
+			case 3:
+				img=1;
+				$(".banner3").fadeOut(1600, function(){
+					$(".banner1").fadeIn(1600);
+				});
+			break;
+		}
+	}, 5000);
+});
